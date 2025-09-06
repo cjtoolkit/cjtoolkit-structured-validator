8c2c36954c3164f48b4232ba10572bebcfcc57e6
Chris Jackson <hello@cjjackson.dev>
2025-09-06 12:47:02 +0100

    fix lib

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@
 //!
 //! Here are the examples of where Poem's i18n and Dioxus's i81n are being used.
 //!
-//! https://github.com/CJ-Jackson/animal_api_again/blob/main/backend_api/src/common/locale/mod.rs
-//! https://github.com/CJ-Jackson/animal_api_again/blob/main/ui/src/common/locale/mod.rs
+//! <https://github.com/CJ-Jackson/animal_api_again/blob/main/backend_api/src/common/locale/mod.rs>  
+//! <https://github.com/CJ-Jackson/animal_api_again/blob/main/ui/src/common/locale/mod.rs>
 //!
 //! You can also extend `Name` and hook in your own RegExp of your choosing, or you could use your
 //! own Post Code Validator; the possibility is endless.
